61c4f6e8a64a83844c069b94c4b4180fa53d13fe
RAHUL <rahulsah.sitentc@gmail.com>
2021-04-14 12:11:34 +0530

    completed

--- a.js
+++ a.js
@@ -1,41 +1,40 @@
-  
-  window.addEventListener('resize',function(){
-      addRequiredclass();
-  })
+window.addEventListener('resize', function(){
+    addRequiredClass();
+})
 
-  
-function addRequiredclass() {
+
+function addRequiredClass() {
     if (window.innerWidth < 860) {
-        document.body.classList.addList.add('mobile')
-    }else {
-        document.body.classList.remove('mobile')
+        document.body.classList.add('mobile')
+    } else {
+        document.body.classList.remove('mobile') 
     }
 }
 
-window.onload = addRequiredclass
+window.onload = addRequiredClass
 
-let hamburger=document.querySelector('.hamburger')
-let mobileNav=document.querySelector('.nav-list')
+let hamburger = document.querySelector('.Hamburger')
+let mobileNav = document.querySelector('.nav-list')
 
-let Bar=document.querySelectorAll('.hamburger span')
+let bars = document.querySelectorAll('.Hamburger span')
 
-let isActive = false 
-hamburger.addEventListener('click',function() {
+let isActive = false
+
+hamburger.addEventListener('click', function() {
     mobileNav.classList.toggle('open')
-
     if(!isActive) {
-
-    
-    bars[0].style.transform = 'rotate(45deg)'
-    bars[1].style.opacity = '0'
-    bars[2].style.transform = 'rotate(-45deg)'
-isActive = true
-    }else {
+        bars[0].style.transform = 'rotate(45deg)'
+        bars[1].style.opacity = '0'
+        bars[2].style.transform = 'rotate(-45deg)'
+        isActive = true
+    } else {
         bars[0].style.transform = 'rotate(0deg)'
         bars[1].style.opacity = '1'
         bars[2].style.transform = 'rotate(0deg)'
         isActive = false
     }
+    
+
 })
 
 
